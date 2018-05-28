8cd7300cca02b88efd02fdee952033d6838c45a1
Indy Mares <indianamares@icloud.com>
2018-05-28 12:10:06 -0700

    new stuff

---
+++ math_quiz_part_one.rb
@@ -0,0 +1,15 @@
+puts "--------------------------------------"
+puts "            MATH QUIZ 1               "
+puts "--------------------------------------"
+puts ""
+puts "Welcome to the math quiz"
+
+print "1. 6 + 7 = "
+
+guess = gets.strip
+
+if guess == "13" then
+ puts " Good job you're correct!"
+else
+  puts "Good try better luck next time."
+end
